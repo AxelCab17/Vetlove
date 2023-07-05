4ffabd43c4c1519a04e572d120822a6ab3f824d9
Allison González <agonzaleza200@gmail.com>
2023-07-05 17:29:43 -0600

    ajustes de messages properties

--- VetLove/src/main/java/com/VetLove/controller/IndexController.java
+++ VetLove/src/main/java/com/VetLove/controller/IndexController.java
@@ -5,17 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author Usuario
- */
 @Controller
 public class IndexController {
-    
     @RequestMapping("/")
     public String page(Model model) {
-        model.addAttribute("attribute", "value");
+        model.addAttribute("mensaje", "");
         return "index";
     }
-    
 }
